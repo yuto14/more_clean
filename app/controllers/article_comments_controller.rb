eb75b5cc6357eadd723e1902e1a4f3fe4307f908
vagrant <vagrant@localhost.localdomain>
2020-07-15 09:21:52 +0000

    [add]コメント削除機能

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -2,7 +2,7 @@ class ArticleCommentsController < ApplicationController
 
     def create
         @article = Article.find(params[:article_id])
-        @article_comment = current_user.ArticleComment.new(article_comment_params)
+        @article_comment = ArticleComment.new(article_comment_params)
         @article_comment.user_id = current_user.id
         @article_comment.article_id = @article.id
         if @article_comment.save
@@ -10,6 +10,13 @@ class ArticleCommentsController < ApplicationController
         else
             render "articles/show"
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @article_comment = ArticleComment.find(params[:id])
+        @article_comment.destroy
+        redirect_to article_path(@article)
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only:[:show, :edit, :update]
   resources :articles do
-    resources :article_comments, only:[:create]
+    resources :article_comments, only:[:create, :destroy]
   end
 
 end
