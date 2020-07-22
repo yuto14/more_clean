3eebfe91438228906769585bc8de1a31db334656
vagrant <vagrant@localhost.localdomain>
2020-07-22 08:47:52 +0000

    [update]コメント機能を非同期通信化

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -6,7 +6,6 @@ class ArticleCommentsController < ApplicationController
         @article_comment.user_id = current_user.id
         @article_comment.article_id = @article.id
         if @article_comment.save
-           redirect_to article_path(@article)
         else
             render "articles/show"
         end
@@ -16,7 +15,6 @@ class ArticleCommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @article_comment = ArticleComment.find(params[:id])
         @article_comment.destroy
-        redirect_to article_path(@article)
     end
 
     private
