924358070d121fe674f7e58b24a4410a6ef3cb85
vagrant <vagrant@localhost.localdomain>
2020-07-27 01:25:13 +0000

    [fix]gem 'acts-as-taggable-on'のマイグレーションファイルを編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_133020) do
+ActiveRecord::Schema.define(version: 2020_07_27_010149) do
 
   create_table "article_comments", force: :cascade do |t|
     t.integer "user_id", null: false
